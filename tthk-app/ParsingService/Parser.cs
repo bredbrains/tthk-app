1399b0807095b0bd449066a06bd9f57a7d68498b
Nikolas Laus <bchik3@gmail.com>
2020-09-23 19:06:35 +0300

    Add Parser class.

---
+++ tthk-app/ParsingService/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using AngleSharp.Dom;
+using AngleSharp.Html.Parser;
+
+namespace tthk_app.ParsingService
+{
+    public class Parser
+    {
+        public static List<string> GetChanges()
+        {
+            List<string> changesTableRows = new List<string>();
+            WebClient client = new WebClient();
+            string doc = client.DownloadString("https://www.tthk.ee/tunniplaani-muudatused/");
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(doc);
+            foreach (IElement element in document.QuerySelectorAll("tr"))
+            {
+                changesTableRows.Add(element.InnerHtml);
+            }
+
+            return changesTableRows;
+        }
+    }
+}
