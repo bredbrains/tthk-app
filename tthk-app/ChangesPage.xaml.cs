6f80fa7a0fd2ae0f41f9602e7cdc6842aba764bd
Nikolas Laus <bchik3@gmail.com>
2020-09-23 20:17:35 +0300

    Parser isn't working, changes will be displayed on these tab.

--- tthk-app/ChangesPage.xaml.cs
+++ tthk-app/ChangesPage.xaml.cs
@@ -1,13 +1,33 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using tthk_app.ParsingService;
+using Xamarin.Forms.Xaml;
 
 namespace tthk_app
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChangesPage
     {
+        private ICommand refreshCommand;
+
         public ChangesPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Parsed.Text = "";
+            foreach (List<string> x in Parser.GetChanges())
+            {
+                foreach (string j in x)
+                {
+                    Parsed.Text += j + " ";
+                }
+            }
+
+            Parsed.Text += "<br>";
         }
     }
 }
--- tthk-app/ParsingService/Parser.cs
+++ tthk-app/ParsingService/Parser.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.Net;
-using AngleSharp.Dom;
 using AngleSharp.Html.Parser;
 
 namespace tthk_app.ParsingService
 {
     public class Parser
     {
-        public static List<string> GetChanges()
+        public static List<List<string>> GetChanges()
         {
-            List<string> changesTableRows = new List<string>();
+            List<List<string>> changesTableRows = new List<List<string>>();
+            List<string> changesRowCells = new List<string>();
             WebClient client = new WebClient();
             string doc = client.DownloadString("https://www.tthk.ee/tunniplaani-muudatused/");
             var parser = new HtmlParser();
             var document = parser.ParseDocument(doc);
-            foreach (IElement element in document.QuerySelectorAll("tr"))
+            foreach (var changesTableRow in document.QuerySelectorAll("tr"))
             {
-                changesTableRows.Add(element.InnerHtml);
+                foreach (var changesTableCell in changesTableRows.)
+                {
+                    changesRowCells.Add(changesTableCell.InnerHtml);
+                }
+
+                changesTableRows.Add(changesRowCells);
             }
 
             return changesTableRows;
