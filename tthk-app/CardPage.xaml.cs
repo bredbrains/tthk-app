31cd6535701bff8a7977d18bfb25e49967fce7e7
Nikolas Laus <bchik3@gmail.com>
2020-09-29 10:36:14 +0300

    Add CardPage for NFC card.

---
+++ tthk-app/CardPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tthk_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardPage
+    {
+        public CardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
