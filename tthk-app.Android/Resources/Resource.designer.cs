48483c8b6a07b4b0b9abf3d075816e21dfe34d5e
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-10-06 21:48:47 +0300

    MainPage to the source view.

--- tthk-app.Android/Resources/Resource.designer.cs
+++ tthk-app.Android/Resources/Resource.designer.cs
@@ -8400,25 +8400,28 @@ namespace tthk_app.Droid
 			public const int notify_panel_notification_icon_bg = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int studentscard = 2131165307;
+			public const int settings = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int telegram = 2131165308;
+			public const int studentscard = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tooltip_frame_dark = 2131165309;
+			public const int telegram = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tooltip_frame_light = 2131165310;
+			public const int tooltip_frame_dark = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int tthklogo = 2131165311;
+			public const int tooltip_frame_light = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int tthklogoapp = 2131165312;
+			public const int tthklogo = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int vk = 2131165313;
+			public const int tthklogoapp = 2131165313;
+			
+			// aapt resource value: 0x7F070082
+			public const int vk = 2131165314;
 			
 			static Drawable()
 			{
--- tthk-app/SettingsPage.xaml.cs
+++ tthk-app/SettingsPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,11 +15,73 @@ namespace tthk_app
         public SettingsPage()
         {
             InitializeComponent();
+            string name = Preferences.Get("name", "none");
+            string group = Preferences.Get("group", "none");
+            if (name != "none")
+            {
+                UserName.Text = name;
+            }
+            else
+            {
+                UserName.Text = null;
+            }
+
+            if (group != "none")
+            {
+                UserGroup.Text = group;
+            }
+            else
+            {
+                UserGroup.Text = null;
+            }
+        }
+
+        private List<string> GenerateGroupEnds()
+        {
+            int nowYear = DateTime.Now.Year;
+            int[] years = new int[] {nowYear, nowYear - 1, nowYear - 2, nowYear - 3};
+            string[] yearsStrings = years.Select(year => year.ToString().Remove(0,2)).ToArray();
+            string[] studyTypes = new string[] {"p", "g"};
+            string[] langs = new string[] {"v", "e"};
+            List<string> ends = new List<string>();
+            foreach (string year in yearsStrings)
+            {
+                foreach (string studyType in studyTypes)
+                {
+                    foreach (string lang in langs)
+                    {
+                        ends.Add(studyType + lang + year);
+                        ends.Add(studyType + lang + year + "-1");
+                        ends.Add(studyType + lang + year + "-2");
+                    }
+                }
+            }
+            return ends;
         }
 
         private void AuthorsCellTapped(object sender, EventArgs e)
         {
             Navigation.PushAsync(new InfoPage());
+        }
+
+        private void UserName_OnCompleted(object sender, EventArgs e)
+        {
+            string value = UserName.Text;
+            Preferences.Set("name", value);
+        }
+
+        private void UserGroup_OnCompleted(object sender, EventArgs e)
+        {
+            string value = UserGroup.Text;
+            var groupEnds = GenerateGroupEnds();
+            if (groupEnds.Any(x=> value.EndsWith(x)))
+            {
+                Preferences.Set("group", value);
+            }
+            else
+            {
+                DependencyService.Get<IMessage>().ShortAlert("Rühm sisestatud valesti.");
+            }
         }
     }
 }
