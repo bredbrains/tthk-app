ae2c866fa125bb129c7954cea44637c95c923ba0
Rostislav <Rostislav@DESKTOP-0GKVI9I>
2020-10-11 19:11:07 +0300

    Timetable added (WebView method)

--- tthk-app/TimetablePage.xaml.cs
+++ tthk-app/TimetablePage.xaml.cs
@@ -1,13 +1,18 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System;
 
 namespace tthk_app
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TimetablePage
+    public partial class TimetablePage : ContentPage
     {
+
         public TimetablePage()
         {
             InitializeComponent();
+            TimeTable.Source = "https://thk.edupage.org/timetable/";
         }
     }
 }
