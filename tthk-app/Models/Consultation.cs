e6a4a54fbd6d7a5a3e25d11583524d914cd95a29
Nikolas Laus <bchik3@gmail.com>
2020-10-17 19:35:33 +0300

    Add Departments struct and consultation class.

--- tthk-app/Models/Consultation.cs
+++ tthk-app/Models/Consultation.cs
@@ -6,5 +6,17 @@ namespace tthk_app.Models
 {
     class Consultation
     {
+        public string Teacher { get; set; }
+        public string Room { get; set; }
+        public List<Dictionary<DayOfWeek, string>> Time;
+        public Department TeacherDepartment;
+
+        public Consultation(string teacher, string room, List<Dictionary<DayOfWeek, string>> time, Department department)
+        {
+            Teacher = teacher;
+            Room = room;
+            Time = time;
+            TeacherDepartment = department;
+        }
     }
 }
